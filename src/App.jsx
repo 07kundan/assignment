b2e68fa28b41e54ead0b924907cb8d1b2b590383
kundan <kundankumar28884@gmail.com>
2024-09-24 13:42:22 +0530

    initialize a project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-red-800">fsaf</div>
+    </>
+  );
+}
+
+export default App;
